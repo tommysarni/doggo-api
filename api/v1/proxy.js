c42f2349c2dbbba3b2c77226656bd04cccb64eab
Tommy Sarni <tsarn1799@gmail.com>
2024-12-03 15:21:29 -0500

    add auth header to proxy request

--- api/v1/proxy.js
+++ api/v1/proxy.js
@@ -7,13 +7,13 @@ dotenv.config();
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
   }
-  
+
   if (req.method !== 'POST') {
     res.statusCode = 405;
     res.end(JSON.stringify({ error: 'Method not allowed.' }));
